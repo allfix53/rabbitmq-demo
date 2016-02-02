14c956653dfd02ad52fdc5c4c52d97989a92ab72
allfix <alfisetyadi@gmail.com>
2016-02-02 08:51:03 +0700

    chage to remote

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 window.onload = function()
 {
-    var socket = io.connect('http://localhost:8080');
+    var socket = io.connect('http://192.168.59.53:8080');
     socket.on('update', function(data) {
         var time = data.minutes + 'm ' + data.seconds + 's';
         document.querySelector('.time').innerHTML = time;
